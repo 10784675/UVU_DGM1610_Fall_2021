e50632267a531f5eb9911a28af7ddcdb9de9b359
Morgan VanWagoner <10784675@uvu.edu>
2021-10-03 20:22:29 -0600

    Added Operations & Variables Practice.

---
+++ Fall 2021/Assets/C# Operations & Variables Practice.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C#Operations&VariablesPractice : MonoBehaviour
+{
+    static void Main(string[] args)
+    {
+        string name = "Morgan";
+        Console.WriteLine(name);
+    }
+}   
+
+{
+    static void Main(string[] args)
+    {
+        int myNum = 15;
+        Console.WriteLine(myNum);
+        }     
+}
+
+{
+    static void Main(string[] args)
+    {
+        int myNum = 15;
+        myNum = 20;
+        Console.WriteLine(myNum);
+    }
+}
+
+{
+    static void Main(string[] args)
+    {
+        int myNum = 5;
+        double myDoubleNum = 5.99D;
+        bool myBool = "true";
+        string myText = "Hello";
+        Console.WriteLine(myNum);
+        Console.WriteLine(myDoubleNum);
+        Console.WriteLine(myBool);
+        Console.WriteLine(myText);
+    }
+}
+
+{
+    static void Main(string[] args)
+    {
+        int myNum = 100000;
+        Console.WriteLine(myNum);
+    }
+}
+
+{
+    static void Main(string[] args)
+    {
+        long myNum = 15000000000L;
+        Console.Writeline(myNum);
+    }
+}
+
+{
+    static void Main(string[] args)
+    {
+        float myNum = 5.75F;
+        Console.WriteLine(myNum);
+    }
+}
+
+{
+    static void Main(string[] args)
+    {
+        double Mynum = 19.99D;
+        Console.WriteLine(Mynum);
+    }
+}
+
+{
+    static void Main(string[] args)
+    {
+        int x = 100 + 50;
+        Console.WriteLine(int);
+    }
+}
+
+{
+    static void Main(string[] args)
+    {
+        int sum1 = 100 + 50;
+        int sum2 = sum1 + 250;
+        int sum3 = sum2 + sum2;
+        Console.WriteLine(sum1);
+        Console.WriteLine(sum2);
+        Console.WriteLine(sum3);
+    }
+}
+
+{
+    static void Main(string[] args)
+    {
+        int x = 10;
+        Console.WriteLine(x);
+    }
+}
+
+{
+    static void Main(string[] args)
+    {
+        int x = 10;
+        x += 5;
+        Console.WriteLine(x);
+    }
+}
+
+{
+    static void Main(string[] args)
+    {
+        int x = 5;
+        Console.WriteLine(x > 3 && x < 10);
+    }
+}
