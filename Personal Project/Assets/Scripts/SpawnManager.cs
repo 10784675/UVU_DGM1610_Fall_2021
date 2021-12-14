5691b02530ba7dc13a715bfc120c4664b2ec8e95
Morgan VanWagoner <10784675@uvu.edu>
2021-12-13 19:48:47 -0700

    Lab 5

--- Personal Project/Assets/Scripts/SpawnManager.cs
+++ Personal Project/Assets/Scripts/SpawnManager.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject[ enemies;
+    public GameObject enemies;
     public GameObject powerup;
+    public GameObject obstacle;
     private float xSpawnRange = 20.0f;
     private float zEnemySpawn = 20.0f;
     private float zPowerupRange = 20.0f;
     private float ySpawn = 0.75f;
-    private float startDelay = 2.0f;
+    private float startDelay = 1.0f;
     private float spawnInterval = 1.5f;
 
     void Start()
     {
         InvokeRepeating("SpawnRandomEnemy", startDelay, spawnInterval);
-        InvokeRepeating("SpawnRandomPowerup", startDelay, spawnInterval);
+        InvokeRepeating("SpawnPowerup", startDelay, spawnInterval);
     }
 
     void Update()
@@ -27,12 +28,17 @@ public class SpawnManager : MonoBehaviour
     {
         float randomX = Random.Range(-xSpawnRange, xSpawnRange);
         float randomZ = Random.Range(-zPowerupRange, zPowerupRange);
-
         Vector3 spawnPos = new Vector3(randomX, ySpawn, zEnemySpawn);
-
-        Instantiate(enemies[randomIndex], spawnPos, enemies[randomIndex].gameObject.transform.rotation);
+        Instantiate(enemies, spawnPos, enemies.gameObject.transform.rotation);
     }
 
+    void SpawnRandomObstacle()
+    {
+        float randomX = Random.Range(-xSpawnRange, xSpawnRange);
+        float randomZ = Random.Range(-zPowerupRange, zPowerupRange);
+        Vector3 spawnPos = new Vector3(randomX, ySpawn, zEnemySpawn);
+        Instantiate(obstacle, spawnPos, obstacle.gameObject.transform.rotation);
+    }
     void SpawnPowerUp()
     {
         float randomX = Random.Range(-xSpawnRange, xSpawnRange);
@@ -40,7 +46,7 @@ public class SpawnManager : MonoBehaviour
 
         Vector3 spawnPos = new Vector3(randomX, ySpawn, zEnemySpawn);
 
-        Instantiate(powerup, spawnPos, powerup.GameObject.transform.rotation);
+        Instantiate(powerup, spawnPos, powerup.gameObject.transform.rotation);
     }
 
 }
